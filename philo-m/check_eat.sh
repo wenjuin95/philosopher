3aa9be5da7029bfed35a1c3d713fb069051ae733
low <lowwenjuin27@gmail.com>
2024-10-21 23:29:17 +0800

    feat: modify the check_eat.sh can input number of philo

--- philo-m/check_eat.sh
+++ philo-m/check_eat.sh
@@ -1,57 +1,38 @@
 #!/bin/bash
 
+# Read the number of philosophers from user input
+read -p "Enter the number of philosophers: " num_philosophers
+
 # Simulate the output from a command
 output=$(cat ./philo.txt)
 
-# Initialize the counter
-eating1=0
-eating2=0
-eating3=0
-eating4=0
-eating5=0
+# Initialize the counters
+declare -a eating_counters
+for ((i=0; i<num_philosophers; i++)); do
+    eating_counters[$i]=0
+done
 
 # Loop through the output
-while read line; # Read the output assign to line
-do 
-    if [[ "$line" == *"1 [0;32mis eating[0m"* ]]; then # Check if the line contains " * is eating"
-        ((eating1++)) 
-    elif [[ "$line" == *"2 [0;32mis eating[0m"* ]]; then
-		((eating2++))
-	elif [[ "$line" == *"3 [0;32mis eating[0m"* ]]; then
-		((eating3++))
-	elif [[ "$line" == *"4 [0;32mis eating[0m"* ]]; then
-		((eating4++))
-	elif [[ "$line" == *"5 [0;32mis eating[0m"* ]]; then
-		((eating5++))
-	fi
-done <<< "$output" # Pass the output to the while loop
+while read line; do
+    for ((i=1; i<=num_philosophers; i++)); do
+        if [[ "$line" == *"$i [0;32mis eating[0m"* ]]; then
+            ((eating_counters[$i-1]++))
+        fi
+    done
+done <<< "$output"
 
-total_eating=$(($eating1 + $eating2 + $eating3 + $eating4 + $eating5))
+# Calculate the total eating count
+total_eating=0
+for ((i=0; i<num_philosophers; i++)); do
+    total_eating=$(($total_eating + ${eating_counters[$i]}))
+done
 
-#total philo had eat
+# Total philo had eat
 echo "Total philo eat: $total_eating"
 
-#philo 1
-if [ $eating1 -ne 0 ]; then # -ne: not equal
-	echo "philo 1 eat: $eating1"
-fi
-
-#philo 2
-if [ $eating2 -ne 0 ]; then
-	echo "philo 2 eat: $eating2"
-fi
-
-#philo 3
-if [ $eating3 -ne 0 ]; then
-	echo "philo 3 eat: $eating3"
-fi
-
-#philo 4
-if [ $eating4 -ne 0 ]; then
-	echo "philo 4 eat: $eating4"
-fi
-
-#philo 5
-if [ $eating5 -ne 0 ]; then
-	echo "philo 5 eat: $eating5"
-fi
+# Print each philosopher's eating count
+for ((i=0; i<num_philosophers; i++)); do
+    if [ ${eating_counters[$i]} -ne 0 ]; then
+        echo "philo $((i+1)) eat: ${eating_counters[$i]}"
+    fi
+done
