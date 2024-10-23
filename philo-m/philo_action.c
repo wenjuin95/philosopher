86905f49557cd3292f49c9e0fa1099169ff68a0c
low <lowwenjuin27@gmail.com>
2024-10-23 22:59:01 +0800

    feat: handle 2 philo when eating to prevent deadlock

--- philo-m/philo_action.c
+++ philo-m/philo_action.c
@@ -23,6 +23,32 @@ void	philo_sleep(t_philo *philo)
 }
 
 /**
+ * @brief handle only two philo is to prevent deadlock
+ * @param philo get philo forks
+ * @note 1. if philo id is even, he will lock the right fork first
+ * @note 2. if philo id is odd, he will lock the left fork first
+*/
+void handle_two_philo_fork(t_philo *philo)
+{
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*r_fork;
+
+	if (philo->philo_id % 2 == 0)
+	{
+		l_fork = philo->right_fork;
+		r_fork = philo->left_fork;
+	}
+	else
+	{
+		l_fork = philo->left_fork;
+		r_fork = philo->right_fork;
+	}
+	(pthread_mutex_lock(l_fork), pthread_mutex_lock(r_fork));
+	philo_say("has taken a fork", philo, philo->philo_id);
+	philo_say("has taken a fork", philo, philo->philo_id);
+}
+
+/**
  * @brief philo take the fork and eat
  * @param philo the philo struct
  * @note 1. if philo only 1, just wait for the time to die
@@ -40,10 +66,15 @@ void	philo_eat(t_philo *philo)
 		usleep_ms(philo->time_to_die);
 		return ;
 	}
-	pthread_mutex_lock(philo->right_fork);
-	pthread_mutex_lock(philo->left_fork);
-	philo_say("has taken a fork", philo, philo->philo_id);
-	philo_say("has taken a fork", philo, philo->philo_id);
+	if (philo->num_philo == 2)
+		handle_two_philo_fork(philo);
+	else
+	{
+		pthread_mutex_lock(philo->right_fork);
+		pthread_mutex_lock(philo->left_fork);
+		philo_say("has taken a fork", philo, philo->philo_id);
+		philo_say("has taken a fork", philo, philo->philo_id);
+	}
 	philo_say(EAT, philo, philo->philo_id);
 	pthread_mutex_lock(philo->do_lock);
 	philo->eating = FLAG_ON;
