6e9e5dbd640e042e937df330a1b8ca9159dafd24
Welow <welow@student.42kl.edu.my>
2024-10-25 13:38:24 +0800

    fix: revert version

--- philo-m/philo_action.c
+++ philo-m/philo_action.c
@@ -23,39 +23,6 @@ void	philo_sleep(t_philo *philo)
 }
 
 /**
- * @brief handle only two philo is to prevent deadlock
- * @param philo get philo forks
- * @note 1. if philo id is even, he will lock the right fork first
- * @note 2. if philo id is odd, he will lock the left fork first
- * @note warning if not implement this function:
- * 		 The warning from ThreadSanitizer indicates a potential 
- *		 deadlock due to lock-order inversion, but it doesn't 
- *		 necessarily mean that a deadlock will always occur. 
- *		 In your test, it seems that the philosophers managed to 
- *		 acquire the forks without deadlocking, but the potential 
- *		 for deadlock still exists.
-*/
-void	handle_two_philo_fork(t_philo *philo)
-{
-	pthread_mutex_t	*l_fork;
-	pthread_mutex_t	*r_fork;
-
-	if (philo->philo_id % 2 == 0)
-	{
-		l_fork = philo->right_fork;
-		r_fork = philo->left_fork;
-	}
-	else
-	{
-		l_fork = philo->left_fork;
-		r_fork = philo->right_fork;
-	}
-	(pthread_mutex_lock(l_fork), pthread_mutex_lock(r_fork));
-	philo_say("has taken a fork", philo, philo->philo_id);
-	philo_say("has taken a fork", philo, philo->philo_id);
-}
-
-/**
  * @brief philo take the fork and eat
  * @param philo the philo struct
  * @note 1. if philo only 1, just wait for the time to die
@@ -68,20 +35,11 @@ void	handle_two_philo_fork(t_philo *philo)
 */
 void	philo_eat(t_philo *philo)
 {
-	if (philo->num_philo == 1)
-	{
-		usleep_ms(philo->time_to_die);
-		return ;
-	}
-	if (philo->num_philo == 2)
-		handle_two_philo_fork(philo);
-	else
-	{
-		pthread_mutex_lock(philo->right_fork);
-		pthread_mutex_lock(philo->left_fork);
-		philo_say("has taken a fork", philo, philo->philo_id);
-		philo_say("has taken a fork", philo, philo->philo_id);
-	}
+
+	pthread_mutex_lock(philo->right_fork);
+	pthread_mutex_lock(philo->left_fork);
+	philo_say("has taken a fork", philo, philo->philo_id);
+	philo_say("has taken a fork", philo, philo->philo_id);
 	philo_say(EAT, philo, philo->philo_id);
 	pthread_mutex_lock(philo->do_lock);
 	philo->eating = FLAG_ON;
@@ -90,6 +48,6 @@ void	philo_eat(t_philo *philo)
 	philo->eating = FLAG_OFF;
 	pthread_mutex_unlock(philo->do_lock);
 	usleep_ms(philo->time_to_eat);
-	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
+	pthread_mutex_unlock(philo->left_fork);
 }
